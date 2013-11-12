d10e12b8cdb0080739abb666bdcde628f8d9b288
changliu <changliu@kanebay.com>
2013-11-12 15:22:38 +0800

    add Build test

---
+++ HelloWeb/src/test/HelloTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void testGreeting() {
+		fail("Not yet implemented");
+	}
+
+}
